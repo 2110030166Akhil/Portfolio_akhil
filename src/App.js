aecbd880cc069568731b6ab3dd831444c18f88d5
2110030166Akhil <2110030166@klh.edu.in>
2024-03-13 12:09:53 +0530

    First Commit
    First Date : 13-03-2024

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { useState, useEffect } from "react";
+import {darkTheme} from '../src/utils/Themes';
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Education from './components/Education';
+import Experience from './components/Experience'
+import Footer from './components/Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Projects from './components/Projects';
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
+function App()
+{
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  return(
+   <ThemeProvider theme={darkTheme}>
+    <Router>
+   <Navbar/>
+   <Body>
+    <Hero/>
+    <Wrapper>
+      <Skills/>
+      <Education/>
+    <Projects openModal={openModal} setOpenModal={setOpenModal}/>
+      <Experience/>
+    </Wrapper>
+    <Footer/>
+   </Body>
+   </Router>
+   </ThemeProvider>
+  );
+}
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,172 @@
+export const Bio = {
+    name: "Akhil Palukuri",
+    description:
+      "Eager B.Tech student at KL University, combining theoretical coursework with practical internship experience to contribute effectively to the IT industry. Possessing strong communication skills, a collaborative mindset, and a passion for learning, I’m enthusiastic about embracing new challenges and making a positive impact.",
+    github: "https://github.com/2110030166Akhil",
+    resume:
+      "https://drive.google.com/file/d/13GxnlSxmq7wfYmyMEe_1NWz0nPOnXuQe/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/akhil-palukuri-b6a147227/",
+    insta: "https://www.instagram.com/akhil__2397/",
+    whats:"https://wa.me/qr/CMWTWGJS3CKA1"
+  };
+  
+  export const skills = [
+    {
+      title:"Programming Languages",
+      skills:[
+        {
+          name:"Java",
+          image:"https://education.oracle.com/file/general/p-80-java.png",
+        },
+        {
+          name:"C",
+          image:"https://www.britefish.net/wp-content/uploads/2019/07/logo-c-1.png",
+        },
+        {
+          name:"Python",
+          image:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png",
+        },
+        {
+           name:"SQL",
+           image:"https://www.stonebranch.com/integration-hub/media/3c/64/66/1636642258/Stonebranch_SQL_Vendor_Product_Logo.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+      {
+      title:"FrameWork",
+      skills:[
+        {
+          name:"React JS",
+          image:"https://miro.medium.com/v2/resize:fit:1200/1*WA_9JsyqFkge2HwYKcdJQw.png",
+        },
+        {
+          name:"Node JS",
+          image:"https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png",
+        },
+        {
+          name:"Django",
+          image:"https://download.logo.wine/logo/Django_(web_framework)/Django_(web_framework)-Logo.wine.png",
+        },
+      ],
+    },
+    {
+      title:"Certifiactions",
+      skills:[
+        {
+          name:"Microsoft Certified:Azure AI Fundamentals",
+          image:"https://yt3.googleusercontent.com/DP2DnSf8hIhdjThIsFyCqktfSgvrLeXfWA0xbPOo8I3n-P2_7c4otmLi6SwbUp1tXcWodn10=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          name:"AWS Cloud Foundations",
+          image:"https://d1.awsstatic.com/Campaigns/Workforce%20Development/aws-academy_275x135.567fe6bc072dbb6246196e0496ddf74b7ec1655b.jpg"
+        },
+        {
+          name:"AWS Machine Learning Foundations",
+          image:"https://d1.awsstatic.com/Campaigns/Workforce%20Development/aws-academy_275x135.567fe6bc072dbb6246196e0496ddf74b7ec1655b.jpg"
+        },
+        {
+          name:"Oracle Cloud Infrastructure 2023 Certified Architect Associate",
+          image:"https://logos-world.net/wp-content/uploads/2020/09/Oracle-Logo.png",
+        },
+        {
+          name:"The Bits and Bytes of Computer Networking(Coursera)",
+          image:"https://about.coursera.org/static/whiteCoursera-23ec484f7091914430ce19b07d09aedf.svg",
+        }
+      ]
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img:"https://media.licdn.com/dms/image/C510BAQEHr27M2nFKcw/company-logo_200_200/0/1630612943934/being_zero_logo?e=2147483647&v=beta&t=adZzSHPAVER2gVs9mQ1Poh1oM4iCcQPhj_XhRYhqAP8",
+      role: "Mentor",
+      company: "Being Zero",
+      date: "January 2024",
+      desc: "Managed mentoring initiatives at Being Zero, overseeing problem-solving processes, providing guidance, and meticulously tracking solved cases in a spreadsheet for analysis.",
+    },
+    {
+      id: 1,
+      img:"https://media.licdn.com/dms/image/C4E0BAQGs7EmZ0yQMpQ/company-logo_200_200/0/1661493099915?e=2147483647&v=beta&t=1UHBrhp9Fy6BawgDK56sdB-cXUS1BzYLcOIy32nZBEY",
+      role: "Web Development Intern",
+      company: "Internpe",
+      date: "June 2023 - July 2023",
+      desc: "It was a one month Internship program where I was trained as Web Developer and henceforth given a project of my own choice to be completed within deadline.",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img:"https://media.collegedekho.com/media/img/institute/logo/KL_Logo_Final_New-pdf_1.jpg",
+      school: "KL University,Hyderabad",
+      date: "2021-2025",
+      grade: "9.41 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at KL University,Hyderabad. I have completed 5 semesters and have a CGPA of 9.41. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems,Data Visualization Techniques,Data Ware House Management,Big Data Analytics,Big Data Optimization and Business Analytics among others."
+    },
+    {
+      id: 1,
+      img: "https://content.jdmagicbox.com/comp/bangalore/m5/080pxx80.xx80.180222224238.x6m5/catalogue/narayana-group-of-educational-institutions-uttarahalli-bangalore-17soaeenqd.jpg",
+      school: "Narayana Junior College",
+      date: "2019-2021",
+      grade: "97.2%",
+      desc: "I completed my class 12 high school education at Narayana Junior College,Hyderabad, where I studied MPC(Maths,Physics,Chemsitry).",
+      degree: "XII,MPC(Maths,Physics,Chemistry)",
+    },
+    {
+      id: 2,
+      img: "https://content.jdmagicbox.com/comp/bangalore/m5/080pxx80.xx80.180222224238.x6m5/catalogue/narayana-group-of-educational-institutions-uttarahalli-bangalore-17soaeenqd.jpg",
+      school: "Narayana Concept School",
+      date: "2018-2019",
+      grade: "9.7 gpa",
+      desc: "I completed my class 10 education at Narayana Concept School,Hyderabad",
+      degree: "X (SSC)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Gas Booking System",
+      description:
+        "Spearheaded the development of a comprehensive gas booking system using Django framework, facilitating seamless management of gas bookings and deliveries",
+      image:"",
+            tags: [
+        "HTML","CSS","Python",
+      ],
+    },
+    {
+      id: 2,
+      title: "Designed Servetless RESTful API",
+      description:
+        "Architected and implemented a Serverless RESTful API using AWS API Gateway and Lambda functions, providing scalable and cost-effective backend solutions for a mobile application",
+      tags: [
+        "AWS lambda","AWs API Gateway","IAM","EC2",
+      ],
+      image:"",
+    },
+    {
+      id: 0,
+      title: "Health Care Website",
+      tags: [
+       "HTML","CSS","JavaScript",
+      ],
+      description:
+        "Designed and developed a user interface for a health care website using HTML, CSS and JavaScript",
+      image:"",
+    },
+];
